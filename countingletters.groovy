1faaae1eb2717a3afe917a943c9180f4c8a7079e
vis.bcn@gmail <vis.bcn@gmail>
2012-10-08 15:08:27 +0100

    Now the program counts any letter introduced by the user

--- countingletters.groovy
+++ countingletters.groovy
@@ -1,10 +1,13 @@
 print ("Insert a phrase: ");
 String phrase = System.console().readLine();
+print ("Insert a letter to count: ");
+String letter = System.console().readLine();
+
 int counter=0;
 for (int i=0;i<phrase.length();i++){
-	if(phrase.charAt(i) == "e"){
+	if(phrase.charAt(i) == letter){
 		counter++;
 	}
 }
-println ("There are " + counter + " e letters in the phrase.");
+println ("There are " + counter + " " + letter + " letters in the phrase.");
 	
