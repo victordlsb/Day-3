3472a5b950196ff737869fea008de41aad467042
vis.bcn@gmail <vis.bcn@gmail>
2012-10-08 15:20:33 +0100

    Palindrome exercise done and working

---
+++ Palindrome.groovy
@@ -0,0 +1,17 @@
+print ("Insert a phrase: ");
+String phrase = System.console().readLine();
+boolean palindrome = true;
+
+for(int i=0;i<phrase.length()/2;i++){
+	if(phrase.charAt(i) != phrase.charAt(phrase.length()-1-i))
+	{	
+		palindrome = false;
+		break;
+	}
+}
+
+if (palindrome){
+	println ("The phrase is a palindrome");
+}else{
+	println ("The phrase is NOT a palindrome");
+}
